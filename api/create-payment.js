7c1d42f087cb2e19dc8548514acd2016924488a1
taxisnelthuis-collab <taxisnelthuis@gmail.com>
2025-11-08 21:16:39 +0100

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     const response = await fetch("https://api.mollie.com/v2/payments", {
       method: "POST",
       headers: {
-        "Authorization": Bearer ${process.env.MOLLIE_API_KEY},
+        "Authorization": "Bearer " + process.env.MOLLIE_API_KEY,
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
